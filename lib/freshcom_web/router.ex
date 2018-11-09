90a64091891e5702c2ac53ee31d7f9a36e2f9b2a
Roy Bao <roy@roybao.com>
2018-11-09 10:50:53 -0800

    Fix: add in content type negotiation plug for jsonapi

--- lib/freshcom_web/router.ex
+++ lib/freshcom_web/router.ex
@@ -1,19 +1,21 @@
 defmodule FreshcomWeb.Router do
   use FreshcomWeb, :router
 
-  pipeline :api do
+  pipeline :jsonapi do
     plug :accepts, ["json-api"]
     plug FreshcomWeb.AuthenticationPlug, ["/v1/token", "/v1/users", "/v1/password_reset_tokens", "/v1/email_verifications", "/v1/password"]
     plug FreshcomWeb.PaginationPlug
+    plug JaSerializer.ContentTypeNegotiation
     plug JaSerializer.Deserializer
   end
 
   scope "/v1/", FreshcomWeb do
-    pipe_through :api
+    post "/token", TokenController, :create
+
+    pipe_through :jsonapi
 
     options "/*path", WelcomeController, :options
 
     resources "/users", UserController, only: [:index, :create, :show, :update, :delete]
-    post "/token", TokenController, :create
   end
 end
